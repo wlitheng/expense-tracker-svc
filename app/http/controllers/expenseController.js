a2ef734d1dfee1d866e0023770d7ed23c8281aed
nintex\wongl <litheng.wong@nintex.com>
2023-02-24 12:48:09 +0800

    chore: enable POST /expenses to accept a list of expenses (hacky way)

--- app/http/controllers/expenseController.js
+++ app/http/controllers/expenseController.js
@@ -6,6 +6,24 @@ const expenseApi = express.Router();
 
 TableStorageClient.init({tableName: 'Expenses'});
 
+function mapRequestToEntity(request){
+    let expense = new AddExpense({
+        id: request.id,
+        date: request.date,
+        month: request.month,
+        year: request.year,
+        title: request.title,
+        description: request.description,
+        user: request.user,
+        category: request.category,
+        amount: request.amount
+    });
+
+    let entity = expense.convertToEntity();
+
+    return {...entity, ...expense};
+};
+
 // GET /expenses/
 expenseApi.get('/', async (req, res) => {
     let expense = new AddExpense({
@@ -19,24 +37,14 @@ expenseApi.get('/', async (req, res) => {
     res.send(expenses).status(200);
 });
 
+// POST /expenses/
 expenseApi.post('/', async (req, res) => {
-    let expense = new AddExpense({
-        id: req.body.id,
-        date: req.body.date,
-        month: req.body.month,
-        year: req.body.year,
-        title: req.body.title,
-        description: req.body.description,
-        user: req.body.user,
-        category: req.body.category,
-        amount: req.body.amount
+    let entities = req.body.expenses.map(mapRequestToEntity);
+    entities.forEach(async entity => {
+        await TableStorageClient.addEntity(entity);
     });
 
-    // store the expenses into storage
-    let entity = expense.convertToEntity();
-    await TableStorageClient.addEntity({...entity, ...expense});
-
-    res.send(expense).status(200);
+    res.send().status(200);
 });
 
 module.exports = expenseApi;
